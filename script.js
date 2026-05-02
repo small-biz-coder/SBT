de1d5b13171b9fc37cf66cac7eaeb9d46acba6aa
Andrew Beiler <andrew@harvestclicks.com>
2026-05-02 14:39:08 -0400

    partials

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+
+//header & footer fetch
+async function loadComponent(selector, file) {
+    const res = await fetch(file);
+    const html = await res.text();
+    document.querySelector(selector).innerHTML = html;
+}
+
+await loadComponent('#header', '/partials/header.html');
+await loadComponent('#footer', '/partials/footer.html');
+
+
+//mobile navigation click, add date to copyright
 const nav = document.querySelector('.navigation');
 const mobileBox = document.querySelector('.mobile-box');
-const copyright = document.querySelector('#copyright');
 
 mobileBox.addEventListener('click', () =>{
 	nav.classList.toggle('mobile-menu-open');
 });
 
+const copyright = document.querySelector('#copyright');
 const currentDate = new Date().getFullYear();
 
 window.addEventListener('load', () => {
